a7bf25676fd4368f391f514411ee5286e16ee684
cartory <xXcartoryXx@gmail.com>
2021-09-24 16:21:58 -0400

    add place_screen

---
+++ lib/src/pages/places/place_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlaceScreen extends StatelessWidget {
+  const PlaceScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      child: const Center(child: Text('Places Screen')),
+    );
+  }
+}
