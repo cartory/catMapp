0207d99ca69f6460d0899efd17847e01f95548f0
cartory <xXcartoryXx@gmail.com>
2021-09-25 23:05:07 -0400

    add global vars

---
+++ lib/src/config.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import 'globals.dart';
+
+final box = GetStorage();
+
+class Routes {
+  static const home = "/home";
+  // auth
+  static const login = "/login";
+  static const profile = "/profile";
+  // tasks module
+  // schedule module
+  // inventory module
+}
+
+final getPages = [
+  GetPage(name: Routes.home, page: () => const HomePage()),
+  // auth
+  GetPage(name: Routes.login, page: () => LoginPage()),
+  // GetPage(name: Routes.profile, page: () => const ProfilePage())
+];
+
+// THEME STYLE
+final themeData = ThemeData(
+  fontFamily: 'Roboto',
+  backgroundColor: const Color(0xffDCE1E6),
+  scaffoldBackgroundColor: const Color(0xffDCE1E6),
+  // colorScheme
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    brightness: Brightness.light,
+    background: const Color(0xffDCE1E6),
+    //
+    primary: const Color(0xff0D27D6),
+    primaryVariant: const Color(0xff4757D5),
+    //
+    secondary: const Color(0xff383D4A),
+    secondaryVariant: const Color(0xffA8877B),
+  ),
+  // BottomNavigationBarThemeData
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    elevation: 0,
+    backgroundColor: Colors.white,
+    selectedItemColor: Color(0xff383D4A),
+    unselectedItemColor: Color(0xff383D4A),
+    type: BottomNavigationBarType.fixed,
+    showSelectedLabels: true,
+    showUnselectedLabels: true,
+    // selected
+    // selectedLabelStyle: TextStyle(color: Colors.white),
+    selectedLabelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    selectedIconTheme: IconThemeData(color: Color(0xffEB008B)),
+    // unselected
+    unselectedLabelStyle: TextStyle(color: Color(0xff383D4A)),
+    unselectedIconTheme: IconThemeData(color: Color(0xff383D4A)),
+  ),
+  // appBar Theme
+  appBarTheme: AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    toolbarHeight: Get.width * .16,
+    backgroundColor: Colors.white,
+    iconTheme: const IconThemeData(
+      size: 20,
+      color: Colors.white,
+    ),
+    titleTextStyle: const TextStyle(
+      fontSize: 18,
+      wordSpacing: 0,
+      letterSpacing: 0,
+      color: Colors.black,
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.bold,
+    ),
+    actionsIconTheme: const IconThemeData(
+      size: 34,
+      color: Colors.grey,
+    ),
+  ),
+  //
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: .3,
+      alignment: Alignment.center,
+      backgroundColor: Colors.white,
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+      textStyle: const TextStyle(
+        fontSize: 14.5,
+        fontFamily: 'Roboto',
+        wordSpacing: 0,
+        letterSpacing: 0,
+      ),
+      primary: const Color(0xaa000000),
+      side: const BorderSide(width: 0, color: Colors.transparent),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+    ),
+  ),
+  // inputdecoration theme
+  inputDecorationTheme: InputDecorationTheme(
+    contentPadding: const EdgeInsets.symmetric(horizontal: 7, vertical: 3),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.transparent),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.transparent),
+    ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.transparent),
+    ),
+  ),
+);
--- lib/src/globals.dart
+++ lib/src/globals.dart
@@ -1,115 +1,14 @@
-import 'package:flutter/material.dart';
+export 'models/user.dart';
 
-import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
+export 'pages/home_page.dart';
+export 'pages/auth/login_page.dart';
 
-import 'pages/home_page.dart';
+export 'pages/auth/home_screen.dart';
+export 'pages/auth/profile_screen.dart';
+export 'pages/places/place_screen.dart';
 
-import 'pages/auth/login_page.dart';
-// import 'pages/auth/profile_screen.dart';
+export 'widgets/text_field.dart';
+export 'widgets/containers.dart';
 
-final box = GetStorage();
-
-class Routes {
-  static const home = "/home";
-  // auth
-  static const login = "/login";
-  static const profile = "/profile";
-  // tasks module
-  // schedule module
-  // inventory module
-}
-
-final getPages = [
-  GetPage(name: Routes.home, page: () => const HomePage()),
-  // auth
-  GetPage(name: Routes.login, page: () => const LoginPage()),
-  // GetPage(name: Routes.profile, page: () => const ProfilePage())
-];
-
-// THEME STYLE
-final themeData = ThemeData(
-  fontFamily: 'Roboto',
-  backgroundColor: const Color(0xffDCE1E6),
-  scaffoldBackgroundColor: const Color(0xffDCE1E6),
-  // colorScheme
-  colorScheme: ColorScheme.fromSwatch().copyWith(
-    brightness: Brightness.light,
-    background: const Color(0xffDCE1E6),
-    //
-    primary: const Color(0xff0D27D6),
-    primaryVariant: const Color(0xff4757D5),
-    //
-    secondary: const Color(0xff383D4A),
-    secondaryVariant: const Color(0xffA8877B),
-  ),
-  // BottomNavigationBarThemeData
-  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-    elevation: 0,
-    backgroundColor: Colors.white,
-    selectedItemColor: Color(0xff383D4A),
-    unselectedItemColor: Color(0xff383D4A),
-    type: BottomNavigationBarType.fixed,
-    showSelectedLabels: true,
-    showUnselectedLabels: true,
-    // selected
-    // selectedLabelStyle: TextStyle(color: Colors.white),
-    selectedLabelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-    selectedIconTheme: IconThemeData(color: Color(0xffEB008B)),
-    // unselected
-    unselectedLabelStyle: TextStyle(color: Color(0xff383D4A)),
-    unselectedIconTheme: IconThemeData(color: Color(0xff383D4A)),
-  ),
-  // appBar Theme
-  appBarTheme: AppBarTheme(
-    elevation: 0,
-    centerTitle: false,
-    toolbarHeight: Get.width * .16,
-    backgroundColor: Colors.white,
-    iconTheme: const IconThemeData(
-      size: 20,
-      color: Colors.white,
-    ),
-    titleTextStyle: const TextStyle(
-      fontSize: 18,
-      wordSpacing: 0,
-      letterSpacing: 0,
-      color: Colors.black,
-      fontFamily: 'Roboto',
-      fontWeight: FontWeight.bold,
-    ),
-    actionsIconTheme: const IconThemeData(
-      size: 34,
-      color: Colors.grey,
-    ),
-  ),
-  //
-  outlinedButtonTheme: OutlinedButtonThemeData(
-    style: OutlinedButton.styleFrom(
-      elevation: .3,
-      alignment: Alignment.center,
-      backgroundColor: Colors.white,
-      primary: const Color(0xaa000000),
-      side: const BorderSide(width: 0, color: Colors.transparent),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-    ),
-  ),
-  // inputdecoration theme
-  inputDecorationTheme: InputDecorationTheme(
-    contentPadding: const EdgeInsets.symmetric(horizontal: 7, vertical: 3),
-    enabledBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(16),
-      borderSide: const BorderSide(color: Colors.transparent),
-    ),
-    focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(16),
-      borderSide: const BorderSide(color: Colors.transparent),
-    ),
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(16),
-      borderSide: const BorderSide(color: Colors.transparent),
-    ),
-  ),
-);
+export 'services/auth.dart';
+export 'services/storage.dart';
