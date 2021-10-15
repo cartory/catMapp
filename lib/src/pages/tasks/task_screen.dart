1e5b1ffbb6df4c23d6f56110591459c129b729fb
cartory <xXcartoryXx@gmail.com>
2021-10-15 15:53:50 -0400

    add task_screen

---
+++ lib/src/pages/tasks/task_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: const Text('Task Screen'),
+    );
+  }
+}
