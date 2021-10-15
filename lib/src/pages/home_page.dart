9314bdbfb07b1e0779a01d22a8829a492bb2608a
cartory <xXcartoryXx@gmail.com>
2021-10-15 15:55:23 -0400

    update home_page > delete FAB

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,15 +1,17 @@
+import 'package:catmapp/src/pages/tasks/task_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:catmapp/src/globals.dart';
 import 'package:catmapp/src/config.dart' show box;
+import 'package:get/get.dart';
 
 Map<int, Widget? Function()> _getCurrentScreen = {
   0: () => const HomeScreen(),
-  3: () => const PlaceScreen(),
-  4: () => ProfileScreen(),
-  1: () => Container(color: Colors.lightGreen, child: const Center(child: Text('Task Screen'))),
+  1: () => const TaskScreen(),
+  2: () => const PlaceScreen(),
+  3: () => ProfileScreen(),
 };
 
 class HomePage extends StatefulWidget {
@@ -21,12 +23,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   User? _user;
-  int _currentIndex = 0;
+  int _currentIndex = 2;
+  final getPlace = Get.put(GetPlace());
 
   @override
   void initState() {
     super.initState();
     _user = User.fromRawJson(box.read<String>('user'));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    getPlace.dispose();
   }
 
   @override
@@ -37,24 +46,14 @@ class _HomePageState extends State<HomePage> {
       extendBody: true,
       bottomNavigationBar: MyBottomBar(
         currentIndex: _currentIndex,
-        items: [
-          const BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
-          const BottomNavigationBarItem(icon: Icon(Icons.checklist_rounded), label: 'tasks'),
-          BottomNavigationBarItem(
-            icon: Transform.translate(
-              offset: const Offset(0, 8),
-              child: FloatingActionButton(
-                child: const Icon(Icons.add),
-                onPressed: () {},
-              ),
-            ),
-            label: '',
-          ),
-          const BottomNavigationBarItem(icon: Icon(Icons.view_module_rounded), label: 'Places'),
-          const BottomNavigationBarItem(icon: Icon(Icons.manage_accounts), label: 'Profile'),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.checklist_rounded), label: 'tasks'),
+          BottomNavigationBarItem(icon: Icon(Icons.view_module_rounded), label: 'Places'),
+          BottomNavigationBarItem(icon: Icon(Icons.manage_accounts), label: 'Profile'),
         ],
         onTap: (index) {
-          if (index != 2 && index != _currentIndex) {
+          if (index != _currentIndex) {
             setState(() => _currentIndex = index);
           }
         },
