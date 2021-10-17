57a8f7b71903d15b5fef93c2f992b49e547fe143
cartory <xXcartoryXx@gmail.com>
2021-10-17 00:18:01 -0400

    update home_page > animated FAB

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,14 @@
-import 'package:catmapp/src/pages/tasks/task_screen.dart';
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+
+import 'package:catmapp/src/pages/tasks/task_screen.dart';
 
 import 'package:catmapp/src/globals.dart';
 import 'package:catmapp/src/config.dart' show box;
-import 'package:get/get.dart';
 
 Map<int, Widget? Function()> _getCurrentScreen = {
   0: () => const HomeScreen(),
@@ -21,14 +24,24 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   User? _user;
   int _currentIndex = 2;
+
   final getPlace = Get.put(GetPlace());
+
+  late final Animation<double> animation;
+  late final AnimationController controller;
 
   @override
   void initState() {
     super.initState();
+
+    animation = Tween<double>(begin: 0, end: 1).animate(
+      controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 150)),
+    );
+
+    if (_currentIndex == 2) controller.forward();
     _user = User.fromRawJson(box.read<String>('user'));
   }
 
@@ -36,10 +49,12 @@ class _HomePageState extends State<HomePage> {
   void dispose() {
     super.dispose();
     getPlace.dispose();
+    controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    print('render homePage');
     return Scaffold(
       // appBar: appBar(),
       body: _getCurrentScreen[_currentIndex]?.call(),
@@ -54,14 +69,37 @@ class _HomePageState extends State<HomePage> {
         ],
         onTap: (index) {
           if (index != _currentIndex) {
+            if (index == 2) controller.forward();
             setState(() => _currentIndex = index);
           }
+        },
+      ),
+      floatingActionButton: floatingActionButton(),
+    );
+  }
+
+  Widget? floatingActionButton() {
+    if (_currentIndex != 2) return null;
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: animation.value,
+          child: child,
+        );
+      },
+      child: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print(_currentIndex);
+          print(getPlace.places.length);
         },
       ),
     );
   }
 
-  PreferredSizeWidget? appBar() {
+  Widget appBar() {
     return AppBar(
       leading: Container(
         alignment: Alignment.center,
@@ -92,7 +130,9 @@ class _HomePageState extends State<HomePage> {
                   size: 33,
                 ),
                 // ignore: avoid_print
-                onPressed: () => print('notification tap'),
+                onPressed: () {
+                  print('notification tap');
+                },
               ),
               const Positioned(
                 top: 23,
