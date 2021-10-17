1edc942738e4be7d473246401ed5030586d9f8fe
cartory <xXcartoryXx@gmail.com>
2021-10-17 03:30:50 -0400

    update containers > add ButtonCard

--- lib/src/widgets/containers.dart
+++ lib/src/widgets/containers.dart
@@ -91,3 +91,61 @@ class _MyBottomBarState extends State<MyBottomBar> {
     );
   }
 }
+
+class ButtonCard extends StatelessWidget {
+  final BorderRadius? borderRadius;
+  final void Function()? onPressed;
+
+  final List<Widget> children;
+
+  final bool isPressed;
+  final double? height;
+  final double? elevation;
+
+  final Color? color;
+  final Color? shadowColor;
+
+  const ButtonCard({
+    Key? key,
+    this.color,
+    this.height,
+    this.elevation,
+    this.onPressed,
+    this.shadowColor,
+    this.borderRadius,
+    this.isPressed = false,
+    this.children = const [],
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: height,
+      height: height,
+      padding: EdgeInsets.zero,
+      margin: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: borderRadius,
+      ),
+      child: InkWell(
+        onTap: onPressed,
+        autofocus: isPressed,
+        borderRadius: borderRadius,
+        splashFactory: InkRipple.splashFactory,
+        child: Card(
+          shadowColor: shadowColor,
+          elevation: isPressed ? 0 : elevation,
+          color: isPressed ? Colors.grey[50] : color,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ?? BorderRadius.zero,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: children,
+          ),
+        ),
+      ),
+    );
+  }
+}
