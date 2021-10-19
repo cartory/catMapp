91bc89973b0e9d4a58c0d0439a49578dc0e1d3dc
cartory <xXcartoryXx@gmail.com>
2021-10-19 15:32:43 -0400

    update place

--- lib/src/models/place.dart
+++ lib/src/models/place.dart
@@ -1,65 +1,54 @@
-// To parse this JSON data, do
-//
-//     final place = placeFromJson(jsonString);
-
 import 'dart:convert';
-
-import 'package:catmapp/src/models/user.dart';
 
 class Place {
   Place({
     this.id,
     this.code,
     this.name,
-    this.description,
-    this.typeid,
-    this.photoUrl,
     this.type,
-    this.users,
-    this.tasks,
     this.places,
+    this.photoUrl,
+    this.description,
   });
 
   int? id;
+
   String? code;
   String? name;
-  String? description;
-  int? typeid;
   String? photoUrl;
+  String? description;
+
   Type? type;
-  List<User>? users;
-  List<dynamic>? tasks;
   List<Place>? places;
+  List<dynamic>? tasks;
 
   factory Place.fromRawJson(String str) => Place.fromJson(json.decode(str));
 
   String toRawJson() => json.encode(toJson());
 
-  factory Place.fromJson(Map<String, dynamic> json) => Place(
-        id: json["id"],
-        code: json["code"],
-        name: json["name"],
-        description: json["description"],
-        typeid: json["Typeid"],
-        photoUrl: json["photoUrl"],
-        type: Type.fromJson(json["type"]),
-        users: List<User>.from(json["users"].map((x) => x)),
-        tasks: List<dynamic>.from(json["tasks"].map((x) => x)),
-        places: json["places"] == null ? null : List<Place>.from(json["places"].map((x) => Place.fromJson(x))),
-      );
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      id: json["id"],
+      code: json["code"],
+      name: json["name"],
+      photoUrl: json["photoUrl"],
+      description: json["description"],
+      type: Type.fromJson(json["type"]),
+      places: json["places"] == null ? null : List<Place>.from(json["places"].map((x) => Place.fromJson(x))),
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "code": code,
-        "name": name,
-        "description": description,
-        "Typeid": typeid,
-        "photoUrl": photoUrl,
-        "type": type!.toJson(),
-        "users": List<User>.from(users!.map((x) => x)),
-        "tasks": List<dynamic>.from(tasks!.map((x) => x)),
-        "places": places == null ? null : List<dynamic>.from(places!.map((x) => x.toJson())),
-      };
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "code": code,
+      "name": name,
+      "photoUrl": photoUrl,
+      "description": description,
+      "type": type?.toJson(),
+      "places": places == null ? null : List.from(places!.map((x) => x.toJson())),
+    };
+  }
 }
 
 class Type {
@@ -75,13 +64,9 @@ class Type {
 
   String toRawJson() => json.encode(toJson());
 
-  factory Type.fromJson(Map<String, dynamic> json) => Type(
-        id: json["id"],
-        name: json["name"],
-      );
+  factory Type.fromJson(Map<String, dynamic> json) {
+    return Type(id: json["id"], name: json["name"]);
+  }
 
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-      };
+  Map<String, dynamic> toJson() => {"id": id, "name": name};
 }
