3e9c1a983e6b0f666f0e8a98598350ba6eac5452
cartory <xXcartoryXx@gmail.com>
2021-10-21 08:01:39 -0400

    update getPlace > full Getx

--- lib/src/getX/get_place.dart
+++ lib/src/getX/get_place.dart
@@ -7,8 +7,75 @@ import 'package:http/http.dart' as http;
 import 'package:catmapp/src/globals.dart' show Place, API_URL;
 
 class GetPlace extends GetxController {
+  final _selectedIndex = 0.obs;
   final _places = <Place>[].obs;
+
+  final _isLoading = true.obs;
+  final _isLoadingChildren = true.obs;
+
   List<Place> get places => _places.value;
+
+  int get selectedIndex => _selectedIndex.value;
+  Place get selectedPlace => places[selectedIndex];
+
+  bool get isLoading => _isLoading.value;
+  bool get isLoadingChildren => _isLoadingChildren.value;
+
+  void setIndex(int index, int placeId) {
+    if (selectedIndex != index) {
+      _selectedIndex.value = index;
+      _isLoadingChildren.value = true;
+      findOne(placeId, index).whenComplete(() {
+        _isLoadingChildren.value = false;
+      });
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    findAll(refresh: true).whenComplete(() {
+      _isLoading.value = false;
+      _isLoadingChildren.value = false;
+    });
+  }
+
+  @override
+  void refresh() {
+    _isLoading.value = true;
+    _isLoadingChildren.value = true;
+
+    findAll(refresh: true).whenComplete(() {
+      _isLoading.value = false;
+      _selectedIndex.value = 0;
+      _isLoadingChildren.value = false;
+    });
+
+    super.refresh();
+  }
+
+  @override
+  void onClose() {
+    _places.close();
+    _isLoading.close();
+    _selectedIndex.close();
+    _isLoadingChildren.close();
+    super.onClose();
+  }
+
+  void seeMorePlaces(int index) {
+    _isLoading.value = true;
+    _isLoadingChildren.value = true;
+
+    _places.value = places[selectedIndex].places ?? [];
+
+    findOne(places[index].id!.toInt(), index).whenComplete(() {
+      _selectedIndex.value = index;
+
+      _isLoading.value = false;
+      _isLoadingChildren.value = false;
+    });
+  }
 
   Future<void> findAll({int page = 0, bool refresh = false}) async {
     try {
@@ -30,15 +97,10 @@ class GetPlace extends GetxController {
 
       final data = json.decode(res.body);
       if (res.statusCode == 500) throw Exception(data['message']);
-
+      
       places[index] = Place.fromJson(data);
     } catch (err) {
       print(err);
     }
-  }
-
-  Future<void> replaceAll(List<Place> subPlaces, int index) async {
-    _places.value = subPlaces;
-    await findOne(subPlaces[index].id!.toInt(), index);
   }
 }
