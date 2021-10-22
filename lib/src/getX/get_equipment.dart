7fbcfd8c13c21e127a1084f1b74c1ecebc5f0eff
cartory <xXcartoryXx@gmail.com>
2021-10-22 17:33:10 -0400

    update getEquipment > setPlace, refresh, findAll

--- lib/src/getX/get_equipment.dart
+++ lib/src/getX/get_equipment.dart
@@ -1,23 +1,62 @@
-// ignore_for_file: avoid_print, invalid_use_of_protected_member
+// ignore_for_file: invalid_use_of_protected_member, avoid_print
+
 import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:catmapp/src/globals.dart' show Equipment, API_URL;
+import 'package:catmapp/src/globals.dart';
 
 class GetEquipment extends GetxController {
+  final _place = Place().obs;
   final _equipments = <Equipment>[].obs;
+
+  final _isLoading = true.obs;
+
+  Place get place => _place.value;
+  bool get isLoading => _isLoading.value;
   List<Equipment> get equipments => _equipments.value;
 
-  Future<void> findAll({int page = 0, int? placeId, bool refresh = false}) async {
+  void setPlace(Place? newPlace) {
+    _place.value = newPlace ?? Place();
+    refresh();
+  }
+
+  @override
+  void refresh() {
+    super.refresh();
+    _isLoading.value = true;
+    findAll(refresh: true).whenComplete(() {
+      _isLoading.value = false;
+    });
+  }
+
+  @override
+  void onClose() {
+    _place.close();
+    _isLoading.close();
+    _equipments.close();
+
+    super.onClose();
+  }
+
+  Future<void> findAll({int page = 0, int limit = 15, bool refresh = false}) async {
     try {
-      final res = await http.get(Uri.parse('$API_URL/equipments?page=$page&placeId=$placeId'));
+      String url = '$API_URL/equipments?page=$page&limit=$limit';
+
+      if (place.id != null) {
+        url += '&placeId=${place.id}';
+      }
+
+      print(url);
+
+      final res = await http.get(Uri.parse(url));
 
       final data = json.decode(res.body);
       if (res.statusCode == 500) throw Exception(data['message']);
 
       if (refresh) equipments.clear();
+
       equipments.addAll(data.map<Equipment>((e) => Equipment.fromJson(e)));
     } catch (e) {
       print(e);
