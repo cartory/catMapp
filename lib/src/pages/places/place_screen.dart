ed318b6a554f0b007487a2a7c866775f9d4038c9
cartory <xXcartoryXx@gmail.com>
2021-10-26 15:25:49 -0400

    update placeScreen

--- lib/src/pages/places/place_screen.dart
+++ lib/src/pages/places/place_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:catmapp/src/config.dart';
 import 'package:catmapp/src/globals.dart';
