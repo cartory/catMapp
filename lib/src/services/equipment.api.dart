1173523b40330a3e4d9ec952c3f20de746d0c721
cartory <xXcartoryXx@gmail.com>
2021-10-27 09:09:28 -0400

    update equipmentApi > findOne

--- lib/src/services/equipment.api.dart
+++ lib/src/services/equipment.api.dart
@@ -6,7 +6,20 @@ import 'package:http/http.dart' as http;
 import 'package:catmapp/src/globals.dart';
 
 class EquipmentApi {
-  // STATICS
+  static Future<Equipment?> findOne(int id) async {
+    try {
+      final res = await http.get(Uri.parse('$API_URL/equipments/$id'));
+      final data = json.decode(res.body);
+
+      if (res.statusCode == 500) throw Exception(data['message']);
+
+      return Equipment.fromJson(data);
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+
   static Future<List<Equipment>> findAll({int? page = 0, int? limit = 20, int? placeId, String? query}) async {
     try {
       String url = '$API_URL/equipments?page=$page&limit=$limit';
