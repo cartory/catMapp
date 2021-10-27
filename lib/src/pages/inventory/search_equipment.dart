6fd48e8907b8387e10272730d8b7817b9bf8ed01
cartory <xXcartoryXx@gmail.com>
2021-10-27 08:39:39 -0400

    update searchEquipment > stream

--- lib/src/pages/inventory/search_equipment.dart
+++ lib/src/pages/inventory/search_equipment.dart
@@ -1,21 +1,52 @@
+// ignore_for_file: file_names
+import 'dart:async';
+
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+
 import 'package:catmapp/src/globals.dart';
+import 'package:catmapp/src/utils/debouncer.dart';
 
 class SearchEquipment extends SearchDelegate<Equipment> {
-  final Equipment? equipment;
+  late final Debouncer<String> _debouncer;
 
-  final equipmentApi = EquipmentApi();
-  SearchEquipment({this.equipment});
+  final _scrollController = ScrollController();
+  late final _equipmentController = StreamController<List<Equipment>>.broadcast();
+
+  Stream<List<Equipment>> get stream => _equipmentController.stream;
+  void Function(List<Equipment>) get sink => _equipmentController.sink.add;
+
+  Place? place;
+  int _page = 0;
 
   @override
   ThemeData appBarTheme(BuildContext context) => Get.theme;
+
+  SearchEquipment([this.place]) {
+    _scrollController.addListener(() async {
+      if (_scrollController.position.maxScrollExtent == _scrollController.position.pixels) {
+        _page++;
+        final newData = await EquipmentApi.findAll(page: _page, query: _debouncer.value);
+
+        if (newData.isNotEmpty) {
+          sink(newData);
+        }
+      }
+    });
+
+    _debouncer = Debouncer<String>(
+      duration: const Duration(milliseconds: 600),
+      onValue: (value) async {
+        sink(await EquipmentApi.findAll(page: _page, query: value, placeId: place?.id));
+      },
+    );
+  }
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: const Icon(Icons.clear),
+        icon: const Icon(Icons.clear_rounded),
         onPressed: () => query = '',
       ),
     ];
@@ -25,38 +56,48 @@ class SearchEquipment extends SearchDelegate<Equipment> {
   Widget? buildLeading(BuildContext context) {
     return IconButton(
       icon: const Icon(Icons.arrow_back_ios),
-      onPressed: () {
-        close(context, Equipment());
-      },
+      onPressed: () => close(context, Equipment()),
     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Text('build Results');
+    throw const Text('show results');
   }
 
-  Widget getSuggestions() {
+  Widget getQuerySuggestions() {
     return StreamBuilder<List<Equipment>>(
-      stream: equipmentApi.dataStream,
-      builder: (context, snap) {
+      stream: stream,
+      builder: (_, snap) {
         if (snap.hasError) {
-          return const Center(
-            child: Text('Error'),
-          );
+          return const Placeholder();
         }
 
         if (!snap.hasData) {
           return Center(
             child: Transform.scale(
               scale: .75,
-              child: CircularProgressIndicator(strokeWidth: 3, color: Get.theme.colorScheme.secondary),
+              child: CircularProgressIndicator(
+                strokeWidth: 3,
+                color: Get.theme.colorScheme.secondary,
+              ),
+            ),
+          );
+        }
+
+        if (snap.data!.isEmpty) {
+          return Center(
+            child: Text(
+              'No Data Found 🔍!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, color: Get.theme.colorScheme.secondary),
             ),
           );
         }
 
         return ListView.builder(
           itemCount: snap.data!.length,
+          controller: _scrollController,
           itemBuilder: (context, index) {
             final equipment = snap.data![index];
 
@@ -65,8 +106,18 @@ class SearchEquipment extends SearchDelegate<Equipment> {
               borderRadius: BorderRadius.zero,
               margin: const EdgeInsets.symmetric(vertical: .1),
               title: equipment.code.toString(),
+              subtitle: Text.rich(TextSpan(
+                text: 'UNIT: ',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(text: '${equipment.unit!.name}\t\t\t\t', style: const TextStyle(fontWeight: FontWeight.normal)),
+                  TextSpan(text: 'STATE: ', children: [
+                    TextSpan(text: '${equipment.state}', style: const TextStyle(fontWeight: FontWeight.normal)),
+                  ]),
+                ],
+              )),
               imageUrl: 'https://i.pinimg.com/736x/6c/92/22/6c922234c15e5d66a3c4ff659cef95d5.jpg',
-              imageDescription: equipment.description,
+              // imageDescription: equipment.description,
               options: [
                 LabelIconButton(iconData: Icons.edit_rounded, label: 'edit', onPressed: () {}),
                 LabelIconButton(iconData: Icons.compare_arrows_rounded, label: 'moves', onPressed: () {}),
@@ -82,10 +133,23 @@ class SearchEquipment extends SearchDelegate<Equipment> {
   @override
   Widget buildSuggestions(BuildContext context) {
     if (query.isNotEmpty) {
-      equipmentApi.query = query;
-      return getSuggestions();
+      _debouncer.value = query;
+      return getQuerySuggestions();
     }
 
-    return Text(query);
+    return Center(
+      child: Text(
+        'Search 🔍!',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20, color: Get.theme.colorScheme.secondary),
+      ),
+    );
+  }
+
+  @override
+  void close(BuildContext context, Equipment result) {
+    _scrollController.dispose();
+    // _equipmentController.close();
+    super.close(context, result);
   }
 }
