11b6be8e693e42d3a09a584c99acc9f41ea0720a
cartory <xXcartoryXx@gmail.com>
2021-10-27 08:38:50 -0400

    update equipmentApi > findAll

--- lib/src/services/equipment.api.dart
+++ lib/src/services/equipment.api.dart
@@ -4,49 +4,15 @@ import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 import 'package:catmapp/src/globals.dart';
-import 'package:catmapp/src/utils/debouncer.dart';
 
 class EquipmentApi {
-  late final Debouncer<String> _debouncer;
-  final _equipmentController = StreamController<List<Equipment>>.broadcast();
-
-  Stream<List<Equipment>> get dataStream => _equipmentController.stream;
-  Function(List<Equipment>) get dataSink => _equipmentController.sink.add;
-
-  set query(query) => _debouncer.value = query;
-
-  EquipmentApi() {
-    _debouncer = Debouncer<String>(
-      duration: const Duration(milliseconds: 500),
-      onValue: (value) async => dataSink(await EquipmentApi.findByQuery()),
-    );
-  }
-
   // STATICS
-  static Future<List<Equipment>> findByQuery() async {
-    try {
-      String url = '$API_URL/equipments';
-      final res = await http.get(Uri.parse(url));
-
-      final data = json.decode(res.body);
-
-      if (res.statusCode == 500) throw Exception(data['message']);
-
-      return List.from(data.map((e) => Equipment.fromJson(e)));
-    } catch (e) {
-      log(e.toString());
-    }
-
-    return [];
-  }
-
-  static Future<List<Equipment>> findAll({int? page = 0, int? limit = 20, int? placeId}) async {
+  static Future<List<Equipment>> findAll({int? page = 0, int? limit = 20, int? placeId, String? query}) async {
     try {
       String url = '$API_URL/equipments?page=$page&limit=$limit';
 
-      if (placeId != null) {
-        url += '&placeId=$placeId';
-      }
+      if (query != null) url += '&query=$query';
+      if (placeId != null) url += '&placeId=$placeId';
 
       final res = await http.get(Uri.parse(url));
       final data = json.decode(res.body);
